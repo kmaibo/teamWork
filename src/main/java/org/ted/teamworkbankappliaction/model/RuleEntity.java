9a6f4bd14565fa734818c7dfff3236ddad17ff99
Olga <prokhorova.ol1115@gmail.com>
2026-05-10 16:36:21 +0300

    Added business logic for dynamic rules with Caffeine caching

--- src/main/java/org/ted/teamworkbankappliaction/model/RuleEntity.java
+++ src/main/java/org/ted/teamworkbankappliaction/model/RuleEntity.java
@@ -2,8 +2,6 @@ package org.ted.teamworkbankappliaction.model;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.GenericGenerator;
-
 import java.util.List;
 import java.util.UUID;
 
