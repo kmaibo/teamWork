7cbd26eee1eae1ef52f2f88533c7be60e8b47dc2
Кирик <kmaibo@mail.ru>
2026-05-13 21:07:56 +0300

    edit

--- src/main/java/org/ted/teamworkbankapplication/telegrambot/TelegramBotApplication.java
+++ src/main/java/org/ted/teamworkbankapplication/telegrambot/TelegramBotApplication.java
@@ -1,4 +1,4 @@
-package pro.sky.telegrambot;
+package org.ted.teamworkbankapplication.telegrambot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
